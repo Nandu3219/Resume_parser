70220c278f5c7ae306a69381d6b1e93c374d0449
Nandu3219 <nandeesha.b@radiantsemi.com>
2024-02-06 18:47:25 +0530

    resume_parser02 is for creating a model give the location of json file which is located in \data\\training\\train_data.json using same model in resume_parser01

--- resume_parser01.py
+++ resume_parser01.py
@@ -1,25 +1,34 @@
 import spacy
 import pickle
 import random
-
-import sys, fitz
+import warnings
+import sys, fitz, os
 
 print(spacy.__version__)
-nlp_model = spacy.load('nlp_model')
+
+warnings.filterwarnings("ignore", category=UserWarning)
+nlp_model = spacy.load('E:\\Nandu\Smartbox\pythonProject\Gsm_testing\output\model-last')
+
+# fname = "Nandeesha_emb_2.0.pdf"
+folder_path = 'Resume_folder'
+files = [f for f in os.listdir(folder_path) if f.endswith('.pdf')]
+pdf_paths = [os.path.join(folder_path, file) for file in files]
+print(pdf_paths)
+
+def get_resume_detailes(file_name):
+    doc = fitz.open(file_name)
+    text = ""
+
+    for page in doc:
+        text = text + str(page.get_text())
+
+    tx = "".join(text.split('\n'))
+    doc = nlp_model(tx)
+
+    for entity in doc.ents:
+        print(f'{entity.label_.upper():{20}}- {entity.text}')
 
 
-fname = "CV_NandeeshaBS_03.pdf"
-doc = fitz.open(fname)
-text = ""
-
-for page in doc:
-    text = text + str(page.get_text())
-
-tx = "".join(text.split('\n'))
-# print(tx)
-
-doc = nlp_model(tx)
-
-for entity in doc.ents:
-    print(f'{entity.label_.upper():{15}}- {entity.text}')
-
+for fname in pdf_paths:
+    get_resume_detailes(fname)
+    print('******************************************************************************************************')
---
+++ resume_parser_02.py
@@ -0,0 +1,69 @@
+import json
+import spacy
+from spacy.tokens import DocBin
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+
+cv_data = json.load(open('E:\\Nandu\\Smartbox\\pythonProject\\Gsm_testing\\CV-Parsing-using-Spacy-3-master\\data\\training\\train_data.json'))
+
+# print(cv_data[0][1])
+
+
+def get_spacy_doc(file, data):
+    nlp = spacy.blank('en')
+    db = DocBin()
+
+    # print(data[0][1])
+    for text,annot in tqdm(data):
+        doc = nlp.make_doc(text)
+
+        # annot = annot["entities"]
+
+        ents = []
+        entity_indices = []
+        # print(annot['entities'])
+        for start, end, label in annot["entities"]:
+            skip_entity = False
+            for idx in range(start, end):
+                if idx in entity_indices:
+                    skip_entity = True
+                    break
+
+            if skip_entity==True:
+                continue
+
+            entity_indices = entity_indices + list(range(start, end))
+
+            try:
+                span = doc.char_span(start, end, label=label, alignment_mode='strict')
+            except:
+                continue
+
+            if span is None:
+                err_data = str([start, end]) + " " + str(text) + '\n'
+                file.write(err_data)
+
+            else:
+                ents.append(span)
+
+        try:
+            doc.ents = ents
+            db.add(doc)
+        except:
+            pass
+
+    return db
+
+train, test = train_test_split(cv_data, test_size=0.3)
+
+file = open('error.txt', 'w',  encoding="utf-8")
+
+db = get_spacy_doc(file, train)
+db.to_disk("train_data.spacy")
+
+db = get_spacy_doc(file, test)
+db.to_disk("test_data.spacy")
+# print(db.tokens)
+file.close()
+
+
